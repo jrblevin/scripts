b698770d0a6059ecca59d57302cb986015ff0054
Jason Blevins <jrblevin@sdf.lonestar.org>
2008-11-26 12:10:30 -0500

    Add header with description and license

--- stats.pl
+++ stats.pl
@@ -1,4 +1,22 @@
 #!/usr/bin/perl
+#
+# A simple web statistics script which processes Apache log files and
+# outputs a short Markdown-formatted list containing various summary
+# statistics.
+#
+# Usage: perl stats.pl /var/log/apache/access.log > stats.text
+#
+# Copyright (C) 2008 Jason Blevins <jrblevin@sdf.lonestar.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
 
 use warnings;
 use strict;
